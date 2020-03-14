48838f7814a54f2efeff3900d2d268419bf34240
Vunk.Lai <vunk.lai@gmail.com>
2020-03-15 00:51:15 +0800

    [enhance] add bootstrap-vue plugin

--- www/src/views/About.vue
+++ www/src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class>
+      <div class="row">
+        <div class="col-md-1">1234</div>
+        <div class="col-md-9">9876</div>
+        <div class="col-md-2">2234</div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style  scoped>
+.row * {
+  border-style: solid;
+}
+</style>
