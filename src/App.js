07a6cc99691e99dc3db70c2d24cc3096eebd9398
Vunk.Lai <vunk.lai@gmail.com>
2021-07-15 18:26:16 +0800

    [clean] Refactory the original App function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return <div className="App">App</div>;
+  }
 }
 
 export default App;
