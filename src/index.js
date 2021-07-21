bd83d3a0b43bcf31d0bb59f6789f49dfce808d32
Vunk.Lai <vunk.lai@gmail.com>
2021-07-21 23:39:22 +0800

    [enhance] Create settings context

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import SettingsProvider from "./settings";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <SettingsProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SettingsProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/settings.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { createContext, useState, useEffect } from "react";
+import { THEMES } from "./constants";
+
+const initialSettings = {
+  compact: true,
+  responsiveFontSizes: false,
+  roundedCorners: true,
+  theme: THEMES.LIGHT,
+};
+
+function SettingsDumps() {
+  // get settings from browser storage
+  try {
+    const dumpedData = window.localStorage.getItem("settings");
+    if (dumpedData) {
+      return JSON.parse(dumpedData);
+    } else {
+      let settings = initialSettings;
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        settings.theme = THEMES.DARK;
+      } else {
+        settings.theme = THEMES.LIGHT;
+      }
+      return settings;
+    }
+  } catch (err) {
+    // JSON.parse failure
+    console.error(new Error(String(err)));
+  }
+}
+
+function SettingsLoads(settings) {
+  // set settings to browser storage
+  window.localStorage.setItem("settings", JSON.stringify(settings));
+}
+
+function SettingsProvider(props) {
+  const { children } = props; // the children in component tree
+  const [settings, setSettings] = useState(initialSettings);
+
+  useEffect(() => {
+    const dumpedSettings = SettingsDumps();
+    if (dumpedSettings) {
+      setSettings(dumpedSettings);
+    }
+  }, []);
+
+  function saveSettings(updatedSettings) {
+    setSettings(updatedSettings);
+    SettingsLoads(updatedSettings);
+  }
+
+  const SettingsContext = createContext({
+    settings: initialSettings,
+    saveSettings: () => {},
+  });
+
+  return (
+    <SettingsContext.Provider value={{ settings, saveSettings }}>
+      {children}
+    </SettingsContext.Provider>
+  );
+}
+
+export default SettingsProvider;
