a1b0cdaef66490683aca2776c17e4aee15c8c4ec
Vunk.Lai <vunk.lai@gmail.com>
2021-07-22 00:21:19 +0800

    [fix] Fix a typo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import SettingsProvider from "./settings";
+import { SettingsProvider } from "./settings";
 
 ReactDOM.render(
   <React.StrictMode>
