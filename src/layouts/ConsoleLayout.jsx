9933aff3909c4f88ff1b913b94a92bb39b883d5d
Vunk.Lai <vunk.lai@gmail.com>
2021-08-18 01:06:22 +0800

    [feat] Keep the landing page simple with Stack component

--- src/layouts/ConsoleLayout.jsx
+++ src/layouts/ConsoleLayout.jsx
@@ -2,70 +2,70 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 import { experimentalStyled } from "@material-ui/core/styles";
-import { makeStyles } from "@material-ui/core/styles";
 
-import Container from "@material-ui/core/Container";
+import Stack from "@material-ui/core/Stack";
+import Divider from "@material-ui/core/Divider";
 import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 
 import CircleIcon from "@material-ui/icons/Circle";
 
-const ConsoleLayoutRoot = experimentalStyled("div")(({ theme }) => ({
-  backgroundColor: theme.palette.background.default,
-  height: "100%",
-  marginTop: "64px",
-}));
-
-const CardItem = experimentalStyled(Card)(({ theme }) => ({
+const StackItem = experimentalStyled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
   textAlign: "center",
-  minHeight: 512 + 128,
+  color: theme.palette.text.secondary,
+  Height: 512,
+  minHeight: 512,
+  minWidth: 512,
   border: "none",
   boxShadow: "none",
 }));
 
-const useStyles = makeStyles({
-  cardHeaderTitle: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
 function ConsoleLayout() {
-  const classes = useStyles();
-
   return (
-    <ConsoleLayoutRoot>
-      <Container fixed>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <CardItem>
-              <CardHeader
-                title={
-                  <div className={classes.cardHeaderTitle}>
-                    <CircleIcon color="disabled" /> Service
-                  </div>
-                }
-              />
-              <CardContent sx={{ display: "inline-block" }}>
-                <Paper
-                  sx={{ width: 512, height: 512, background: "Gainsboro" }}
-                ></Paper>
-              </CardContent>
-            </CardItem>
-          </Grid>
-          <Grid item xs={6}>
-            <CardItem>
-              <Outlet />
-            </CardItem>
-          </Grid>
-        </Grid>
-      </Container>
-    </ConsoleLayoutRoot>
+    <Stack
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+      divider={<Divider orientation="vertical" flexItem />}
+      sx={{ paddingTop: 16 }}
+    >
+      <StackItem>
+        <Card
+          align="center"
+          sx={{
+            boxShadow: "none",
+            backgroundColor: "",
+            height: 512,
+            width: 512,
+          }}
+        >
+          <CardHeader
+            title={
+              <div>
+                <CircleIcon color="disabled" /> Service
+              </div>
+            }
+          />
+          <CardContent>
+            <Paper
+              sx={{
+                width: 256 + 128,
+                height: 256 + 128,
+                background: "LightGray",
+              }}
+            ></Paper>
+          </CardContent>
+        </Card>
+      </StackItem>
+      <StackItem>
+        <Outlet />
+      </StackItem>
+    </Stack>
   );
 }
 
